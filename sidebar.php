d1296c6fa9353b6bfc4ed8561f451630f36096e6
awcooper <wesner.cooper@gmail.com>
2019-02-12 10:53:10 -0500

    inital commit

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<div class="col-sm-3 blog-sidebar">
+	<div class="sidebar-module">
+		<div class="sidebar-module-inset">
+			<h4>Archives</h4>
+			<ol class="list-unstyled">
+				<ol class="list-unstyled">
+					<?php wp_get_archives( 'type=monthly' ); ?>
+				</ol>
+			</ol>
+		</div>
+	</div>
+	<div class="sidebar-module">
+		<div class="sidebar-module-inset">
+			<h4>Social Media</h4>
+			<ol class="list-unstyled">
+				<li><a href="#">GitHub</a></li>
+				<li><a href="#">Instagram</a></li>
+				<li><a href="#">Linkedin</a></li>
+			</ol>
+		</div>
+	</div>
+</div><!-- /.blog-sidebar -->
